c500da99be7eb66464b063e95ee08133f67f8937
ZLY <ZLY@DESKTOP-M34DMKC>
2019-06-03 09:23:08 +0800

    a

--- TestGit/src/a.java
+++ TestGit/src/a.java
@@ -1,4 +1,52 @@
 
 public class a {
 
+		private Integer id;
+		private String name;
+		private String pwd;
+		private String lasttime;
+		private String ip;
+
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getPwd() {
+			return pwd;
+		}
+
+		public void setPwd(String pwd) {
+			this.pwd = pwd;
+		}
+
+		public String getLasttime() {
+			return lasttime;
+		}
+
+		public void setLasttime(String lasttime) {
+			this.lasttime = lasttime;
+		}
+
+		public String getIp() {
+			return ip;
+		}
+
+		public void setIp(String ip) {
+			this.ip = ip;
+		}
+
+
 }
